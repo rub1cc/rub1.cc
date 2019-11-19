2eccb4c9265ee1233908237b3dce303f81d34d79
Farazaulia <raz.muhammad14@gmail.com>
2019-11-19 15:46:55 +0700

    add image and change styling

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTwitter, faGithub, faLinkedin} from '@fortawesome/free-brands-svg-icons'
+import { faTwitter, faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 import { faPencilAlt, faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import '../styles.scss';
 
@@ -11,26 +11,29 @@ class Index extends Component {
 
   render() {
     return (
-      <main className="jsx-2756927243">
+      <main>
         <section>
-          <h2>👋 Hi, I am</h2>
-          <h1>Faraz</h1>
+          <img className="my-photo" src="/static/me.jpg" alt="Farazaulia photo" />
+          <div>
+            <h1>👋 Hi, My name is <u>Faraz</u></h1>
+            <h2>I am a Software Engineer</h2>
+          </div>
         </section>
         <nav>
           <a href="https://twitter.com/farazaulia" target="_blank">
-            <FontAwesomeIcon icon={faTwitter}/> Tweets
+            <FontAwesomeIcon icon={faTwitter} /> Tweets
           </a>
           <a href="https://medium.com/@farazaulia" target="_blank">
-            <FontAwesomeIcon icon={faPencilAlt}/> Writings
+            <FontAwesomeIcon icon={faPencilAlt} /> Writings
           </a>
           <a href="https://github.com/farazaulia" target="_blank">
-            <FontAwesomeIcon icon={faGithub}/> Code
+            <FontAwesomeIcon icon={faGithub} /> Code
           </a>
           <a href="https://linkedin.com/in/farazaulia" target="_blank">
-            <FontAwesomeIcon icon={faLinkedin}/> More
+            <FontAwesomeIcon icon={faLinkedin} /> More
           </a>
           <a href="mailto:raz.muhammad14@gmail.com">
-            <FontAwesomeIcon icon={faEnvelope}/> Get in touch!
+            <FontAwesomeIcon icon={faEnvelope} /> Get in touch!
           </a>
         </nav>
       </main>
