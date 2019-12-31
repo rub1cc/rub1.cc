f03bd776210e398aa7250c284c3e6cffa2a325c6
Farazaulia <raz.muhammad14@gmail.com>
2019-12-31 10:38:42 +0700

    Update bio

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
-import React, { Component } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTwitter, faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
-import { faPencilAlt, faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faLinkedin, faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope, faPencilAlt } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Head from "next/head";
+import React, { Component, Fragment } from 'react';
 import '../styles.scss';
 
 class Index extends Component {
@@ -11,32 +12,45 @@ class Index extends Component {
 
   render() {
     return (
-      <main>
-        <section>
-          <img className="my-photo" src="/static/me.jpg" alt="Farazaulia photo" />
-          <div>
-            <h1>👋 Hi, My name is <u>Faraz</u></h1>
-            <h2>I am a software engineer.</h2>
-          </div>
-        </section>
-        <nav>
-          <a href="https://twitter.com/farazaulia" target="_blank">
-            <FontAwesomeIcon icon={faTwitter} /> Tweets
+      <Fragment>
+        <Head>
+          <title>Faraz Aulia - Mobile &amp; Web App Developer</title>
+        </Head>
+        <main>
+          <section>
+            <img className="my-photo" src="/static/me.jpg" alt="Farazaulia photo" />
+            <div>
+              <h1>👋 Hi, My name is <u>Faraz</u></h1>
+              <h2>I am a Mobile &amp; Web App Developer.</h2>
+              <br/>
+              <h2 className="fan-of">
+                Big fan of
+                <span id="react-native" className="hashtag">React Native</span>
+                <span id="nextjs" className="hashtag">NextJS</span>
+                <span id="redux" className="hashtag">Redux</span>
+                <span id="sass" className="hashtag">Sass</span>
+              </h2>
+            </div>
+          </section>
+          <nav>
+            <a href="https://twitter.com/farazaulia" target="_blank">
+              <FontAwesomeIcon icon={faTwitter} /> Tweets
           </a>
-          <a href="https://medium.com/@farazaulia" target="_blank">
-            <FontAwesomeIcon icon={faPencilAlt} /> Writings
+            <a href="https://medium.com/@farazaulia" target="_blank">
+              <FontAwesomeIcon icon={faPencilAlt} /> Writings
           </a>
-          <a href="https://github.com/farazaulia" target="_blank">
-            <FontAwesomeIcon icon={faGithub} /> Code
+            <a href="https://github.com/farazaulia" target="_blank">
+              <FontAwesomeIcon icon={faGithub} /> Code
           </a>
-          <a href="https://linkedin.com/in/farazaulia" target="_blank">
-            <FontAwesomeIcon icon={faLinkedin} /> More
+            <a href="https://linkedin.com/in/farazaulia" target="_blank">
+              <FontAwesomeIcon icon={faLinkedin} /> More
           </a>
-          <a href="mailto:raz.muhammad14@gmail.com">
-            <FontAwesomeIcon icon={faEnvelope} /> Get in touch!
+            <a href="mailto:raz.muhammad14@gmail.com">
+              <FontAwesomeIcon icon={faEnvelope} /> Get in touch!
           </a>
-        </nav>
-      </main>
+          </nav>
+        </main>
+      </Fragment>
     );
   }
 }
